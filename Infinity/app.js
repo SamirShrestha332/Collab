60b799693339173e5ead4a5f162b1ceacda7d082
SamirShrestha332 <119655213+SamirShrestha332@users.noreply.github.com>
2024-04-03 08:44:58 +0545

    Add files via upload

--- Infinity/app.js
+++ Infinity/app.js
@@ -31,31 +31,12 @@ function closeNav() {
 }
 
 
-const navSlide= () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('ul');
-  const crossicon = document.querySelector('.');
-  let navlinks = document.querySelectorAll('ul li')
- 
-  .
-  burger.addEventListener('click',() => {
-     //toggle nav
-     nav.classList.toggle('nav-active');
-     //animation links
-  navlinks.forEach((link, index) => {
-   if(link.style.animation){
-     link.style.animation=''
-   }
-   else{
-     link.style.animation = `animationnav 0.3s ease forwards ${index/7+0.5}s`;
-     //this index/7 is a basically use for a smooth delay different making  in animation
-   }
- });
- burger.classList.toggle('toggle');
-});
 
 
-  } 
-navSlide()
+let men_Btn=document.querySelector('.menindicator');
 
+men_Btn.addEventListener('click',()=>{
+ window.location="/Navgation/nav.html";
+  men_Btn.style.color='rgb(184, 73, 165);'
 
+})
